241a84cdd9ea64e548b36801e9f693a984070fc2
zobinHuang <zobin1999@gmail.com>
2024-03-25 06:46:26 +0800

    chore

---
+++ chapter_5/build/CMakeFiles/topk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/topk"
+  "../bin/topk.pdb"
+  "CMakeFiles/topk.dir/profiling/topk/main.cu.o"
+  "CMakeFiles/topk.dir/profiling/topk/main.cu.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/topk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
